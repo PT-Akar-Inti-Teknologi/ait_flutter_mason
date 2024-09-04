cf506496602ec43660a360f047cafd4a01ac5fff
Aditya Hastungkoro Hadi <aditya.hadi@akarinti.tech>
2024-09-04 15:27:25 +0700

    refactor: Change exporter file name to _file_name.dart

---
+++ ait_new_package/__brick__/{{name.snakeCase()}}/lib/_{{name.snakeCase()}}.dart
@@ -0,0 +1,3 @@
+export 'src/domain/_domain.dart';
+export 'src/presentation/_presentation.dart';
+export '{{name.snakeCase()}}_module.dart';
--- ait_new_package/__brick__/{{name.snakeCase()}}/lib/{{name.snakeCase()}}.dart
+++
@@ -1,3 +0,0 @@
-export 'src/domain/{{name.snakeCase()}}_domain.dart';
-export 'src/presentation/{{name.snakeCase()}}_presentation.dart';
-export './{{name.snakeCase()}}_module.dart';
